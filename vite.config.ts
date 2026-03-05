8aa8bf829bea5f7623b03e8a16ee02c94d65dc30
Ricardo Martins <ric.martinodev@gmail.com>
2026-03-05 16:42:40 -0300

    atualizado

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/Iron-Fitness/',
     plugins: [react(), tailwindcss()],
     build: {
       outDir: 'docs',
